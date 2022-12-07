5a93d79d95a652fff623d0e7de9273214518c443
Abel S. Santamarina <89417559+asantamarina@users.noreply.github.com>
2022-12-06 23:33:51 -0500

    Add files via upload

---
+++ 2022/day4/day4p2.py
@@ -0,0 +1,25 @@
+#Day4
+#Abel Santamarina
+#AdventOfCode2022
+
+with open('inputd4.txt') as day4input:
+    totalsections = day4input.read().strip('\n').split('\n')
+    
+WrongAssignments = []
+
+for section in totalsections:
+    section1 = section.split(',')[0]
+    section2 = section.split(',')[1] 
+    lowend1 = int(section1.split('-')[0])
+    highend1 = int(section1.split('-')[1])
+    lowend2 = int(section2.split('-')[0])
+    highend2 = int(section2.split('-')[1])
+    
+    Elf1 = set(range(lowend1,highend1 + 1))
+    Elf2 = set(range(lowend2,highend2 + 1))
+    
+    WrongAssignments.append(Elf1.isdisjoint(Elf2))
+    
+print(WrongAssignments.count(False))
+    
+    
